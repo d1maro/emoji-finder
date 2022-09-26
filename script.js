a7fac575e84850e85b21236ab1a1a06141a3a0b4
student1 <dima.romakhov@gmail.com>
2022-09-26 17:41:20 +0300

    emodji module added

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 "use strict";
 
-const emojiArr = [
-    {content: "🔢", head: "1234", text: "input symbol for numbers symbol"},
-    {content: "🔢", head: "1234", text: "input symbol for numbers symbol"},
-    {content: "🔢", head: "1234", text: "input symbol for numbers symbol"},
-];
+import { data } from "./emoji.js"
 
 const cont = document.querySelector(".grid__container");
 
-function createCard(emojiArr) {
+function createCard(data) {
 const card = document.createElement("div");
 card.className = "grid__card";
 
 const emoji = document.createElement("div");
 emoji.className = "card__emoji";
-emoji.textContent = emojiArr.content;
+emoji.textContent = data.symbol;
 
 const title = document.createElement("h2");
 title.className = "card__title";
-title.textContent = emojiArr.head;
+title.textContent = data.title;
 
 const description = document.createElement("p");
 description.className = "card__description";
-description.textContent = emojiArr.text;
+description.textContent = data.keywords;
 
 card.append(emoji);
 card.append(title);
@@ -31,6 +27,6 @@ card.append(description);
 return card;
 }
 
-emojiArr.forEach(elem => {
+data.forEach(elem => {
     cont.append(createCard(elem));
 })
